21074590f92f10a92cb9d7aec1d8087623e1b04a
Dmitry Pakhomov <skittree@gmail.com>
2023-12-10 04:42:29 +0500

    try other model

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from transformers import AutoProcessor, BarkModel
+from transformers import VitsTokenizer, VitsModel
 import streamlit as st
+import torch
+
+MODEL_ID = "facebook/mms-tts-eng"
+
 
 @st.cache_resource
 def load_model():
-    print('sasdasdasdasd')
-    model = BarkModel.from_pretrained("suno/bark-small")
+    model = VitsModel.from_pretrained(MODEL_ID)
     return model
 
 @st.cache_resource
 def load_processor():
-    processor = AutoProcessor.from_pretrained("suno/bark-small")
+    processor = VitsTokenizer.from_pretrained(MODEL_ID)
     return processor
 
 def preprocess_text(text):
     processor = load_processor()
-    output = processor(text)
+    output = processor(text=text, return_tensors="pt")
     return output
 
 def generate_tts(text):
@@ -22,14 +25,14 @@ def generate_tts(text):
     model = load_model()
 
     with st.spinner('Генерируется аудио...'):
-        speech_output = model.generate(**inputs)
+        with torch.no_grad():
+            outputs = model(**inputs)
 
-        rate = model.generation_config.sample_rate
-        audio = speech_output[0].cpu().numpy()
+        waveform = outputs.waveform[0]
+        return waveform, model.config.sampling_rate
 
-        return [audio, rate]
-    
-st.title('Text-to-Speech с аннотациями')
+
+st.title('Text-to-Speech 🇺🇸')
 text = st.text_area('Введите текст')
 result = st.button('Произнести')
 if result and text:
